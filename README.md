c1871662ae8f1093b1c7ec9029f0df151774e06a
Sokaina <sb575@inlumine.ual.es>
2022-04-15 12:29:13 +0200

    Version 1 polinomio done

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# ProyectoPolinomioSR
-Este proyecto realizado por Sokaina Baouch y Ruth Rodríguez trata de una herramienta para el cálculo de polinomios y sistemas de ecuaciones.
+# Operaciones entre polinomios
+
+_En este código, se crea una calculadora para dos polinomios, con suma y resta entre dos polinomios_
+
+## Ejecución
+
+Este código, se ejecuta desde el main.py.
+
+```
+python main.py
+```
+Se debe ingresar un valor entre 0 y 2 para escoger una opción del menú:
+- *1*, para sumar polinomios
+- *2*, para restar polinomios
+- *0*, para salir
+
+
+después, se debe ingresar la cantidad de coeficientes que tendrá cada polinomio.
+
+
+Ejemplo de parámetros de entrada:
+
+```
+Opción: 1
+Ingrese el número de elementos del primer polinomio: 3
+Ingresa el número de elementos del segundo polinomio: 2
+Ingrese los elementos del primer polinomio: 1,2,3
+Ingrese los elementos del segundo polinomio: 2,2
---
+++ main.py
@@ -0,0 +1,46 @@
+
+from polinomio import calc
+
+calculo = calc()
+
+def menu():
+    print("*******       CALCULADORA       *******")
+    print("*  Selecciona una opcion:             *")
+    print("*  1. Suma                            *")
+    print("*  2. Resta                           *")
+    print("*  0. Salir                           *")
+    print("***************************************")
+    
+    opcion = input("Opcion: ")
+
+    try:
+        if int(opcion) >= 0 and int(opcion) <= 2:
+            return(int(opcion))
+        else:
+            print("Opcion incorrecta, por favor ingrese una opcion (1 para sumar y 2 para restar): ")
+            return(int(input("Opcion: ")))
+    except:
+        print("Opcion incorrecta, por favor ingrese una opcion (1 para sumar y 2 para restar): ")
+        return(int(input("Opcion: ")))
+
+while(True):
+    opcion = menu()
+
+    if opcion == 0:
+        print("Fin")
+        break
+
+    else:
+        
+        num11 = int(input("Ingrese el numero de elementos del primer polinomio: ")) 
+        num22 = int(input("Ingresa el numero de elementos del segundo polinomio: "))
+
+        num1 = list(map(int,input("\nIngrese los elementos del primer polinomio: ").strip().split(',')))[:num11]
+        num2 = list(map(int,input("\nIngrese los elementos del segundo polinomio: ").strip().split(',')))[:num22]
+
+        if opcion == 1:
+            calculo.suma(num1,num2)
+        elif opcion == 2:
+            calculo.resta(num1,num2)
+        else:
+            print("No es posible realizar la operacion, por favor ingrese una opcion de las disponibles en el menu")
